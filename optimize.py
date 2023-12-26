1814e0769191a6b406196eb84dcd8a3c202fc003
Eric Wang <wange519@gmail.com>
2023-12-25 21:14:33 -0500

    Saving model

--- optimize.py
+++ optimize.py
@@ -41,12 +41,6 @@ class NeuralNetwork(nn.Module):
 model = NeuralNetwork()
 #print(model)
 
-learning_rate = 1e-3
-batch_size = 64
-epochs = 5
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
 def train_loop(dataloader, model, loss_fn, optimizer):
     size = len(dataloader.dataset)
     # Set the model to training mode - important for batch normalization and dropout layers
@@ -87,9 +81,16 @@ def test_loop(dataloader, model, loss_fn):
     correct /= size
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-epochs = 10
+learning_rate = 1e-3
+batch_size = 64
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+epochs = 20
+
 for t in range(epochs):
     print(f"Epoch {t+1}\n-------------------------------")
     train_loop(train_dataloader, model, loss_fn, optimizer)
     test_loop(test_dataloader, model, loss_fn)
 print("Done!")
+
+torch.save(model.state_dict(), 'model_weights.pth')
